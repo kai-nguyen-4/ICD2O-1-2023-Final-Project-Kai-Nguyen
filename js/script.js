64d8a41c3fa34bd176382a78c6d797dfb1a2c378
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-05-29 18:49:50 +0000

    added button js

--- js/script.js
+++ js/script.js
@@ -16,6 +16,8 @@ if (navigator.serviceWorker) {
 
 let score = 0
 let highScore = 0
+let hiddenNumber = Math.floor(Math.random() * 100) + 1
+let tries = 5
 
 function updateScore() {
   // save to local storage
@@ -27,3 +29,29 @@ function updateScore() {
   document.getElementById("highScore").innerHTML = "High Score: " + highScore
 }
 
+function buttonClicked() {
+  const userNumber = parseInt(document.getElementById("number-entered").value)
+  console.log(userNumber)
+  console.log(hiddenNumber)
+  if ((userNumber >= 1) && (userNumber <= 100)) {
+    if (userNumber == hiddenNumber) {
+      score++
+      document.getElementById("answer").innerHTML += "Question " + score + " correct!<br />"
+      tries = 5
+      if (score > highScore) {
+        highScore == score
+      }
+      hiddenNumber = Math.floor(Math.random() * 100) + 1
+    } else if (userNumber > hiddenNumber) {
+      document.getElementById("hint").innerHTML = "Incorrect, the number is lower."
+      tries--
+    } else if (userNumber < hiddenNumber) {
+      document.getElementById("hint").innerHTML = "Incorrect, the number is higher."
+      tries--
+    }
+    if (tries == 0) {
+      document.getElementById("answer").innerHTML = "Game over! The number was: " + hiddenNumber + ". Your score is " + score
+    }
+    document.getElementById("tries").innerHTML = "Tries remaining: " + tries
+  }
+}
